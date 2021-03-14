b29e98af0aa8c8d956cc36e544d76b4e738d9d67
nunomiguelcg <nunomiguel.carvalhogoncalves@gmail.com>
2021-03-14 13:36:16 +0000

    Finnished transcribing command and event handlers. Started to transcribe events to seperate files. Finnished trancribing ready event.

---
+++ events/client/ready.js
@@ -0,0 +1,3 @@
+module.exports = (client, Discord) => {
+    console.log('The bot has logged in!');
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
 require('dotenv').config();
 
@@ -15,7 +15,7 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
-    require(`.handlers/${handler}`)(client, Discord);
+    require(`./handlers/${handler}`)(client, Discord);
 })
 
 //const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
